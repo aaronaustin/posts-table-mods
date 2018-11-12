d6e187feeae7d70acc34240bd78454f9fdd56b5d
Aaron Austin <aaron@aaronaustin.com>
2018-11-12 09:17:49 -0500

    add custom category buttons

--- posts-table-mods.php
+++ posts-table-mods.php
@@ -1,7 +1,7 @@
 <?php
 /*Plugin Name: Post Table Mods
 Description: Modifies the main posts table.
-Version: 1.0.5
+Version: 1.0.6
 License: GPLv2
 GitHub Plugin URI: https://github.com/aaronaustin/posts-table-mods
 */
